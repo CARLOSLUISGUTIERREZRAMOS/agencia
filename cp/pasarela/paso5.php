8256e2384049be9f7c1280b05bf022cd0de93695
carlos luis gutierrez ramos <carlos5t@hotmail.com>
2019-07-23 16:04:12 -0500

    $url - dinamic

--- cp/pasarela/paso5.php
+++ cp/pasarela/paso5.php
@@ -184,10 +184,10 @@
                             $_SESSION['registro_id'] = $registro;
                             $_SESSION['token_seguridad_visa'] = $token;
                         ?> 
-                        <form  action="metodos_pago.php" id="form_visa" method='post' style="display: none">
                             <?php
                                 require_once("../../config.php");
-                            ?>
+                                ?>
+                            <form  action="<?=$url?>/cp/pasarela/metodos_pago.php" id="form_visa" method='post' style="display: none">
                             <script src='<?=$libreriaJsVisa?>'
                                 data-sessiontoken='<?= $objSessionVisa->sessionKey ?>'
                                 data-channel='web'
